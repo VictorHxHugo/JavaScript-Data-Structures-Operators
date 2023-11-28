5d4c56be459b2472b8395a0f65ba52002dc6b2b9
VictorHxHugo <victorzk6@hotmail.com>
2023-11-28 13:15:20 -0300

    Looping Objects: Object Keys, Values, and Entries

--- script.js
+++ script.js
@@ -49,6 +49,31 @@ const restaurant = {
   },
 };
 
+// Looping Objects: Object Keys, Values, and Entries
+
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+//  Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+//  [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`In ${day} we open at ${open} and close at ${close}`);
+}
+/*
 // Optional Chaining (?.)
 
 // Wrong way
@@ -72,6 +97,7 @@ console.log(restaurant.orderRisotto?.(0, 1) ?? 'Merthods does no exist');
 // Arrays
 const users = [{ name: 'Victor', email: 'victorzk6@hotmail.com' }];
 console.log(users[0]?.name ?? 'Users array empty');
+*/
 
 /*
 // The for-of Loop
