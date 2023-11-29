65ce6c33325f2a8f6cc619d0d79f8b3cda8a91aa
VictorHxHugo <victorzk6@hotmail.com>
2023-11-29 14:17:33 -0300

    Maps: Fundamentals

--- script.js
+++ script.js
@@ -47,7 +47,42 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+///////////////////////////////////////
+// Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
 
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+/*
+///////////////////////
+// SET
 const orderSet = new Set([
   'Pasta',
   'Pizza',
@@ -76,6 +111,7 @@ console.log(staffUnique);
 
 console.log(new Set(staffUnique).size);
 console.log(new Set('victorhugo').size);
+*/
 
 /*
 // Looping Objects: Object Keys, Values, and Entries
