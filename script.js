d0321533318801b287dedd6c48f3024ecd6ec483
VictorHxHugo <victorzk6@hotmail.com>
2023-11-29 14:52:17 -0300

    Maps: Iteration

--- script.js
+++ script.js
@@ -47,6 +47,43 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+///////////////////////////////////////
+// Maps: Iteration
+
+const question = new Map([
+  ['question', 'What is the best test programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScrippt'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try again'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+console.log(hoursMap.get('fri'));
+
+// Quiz app
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer: ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]); // = console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+/*
 ///////////////////////////////////////
 // Maps: Fundamentals
 const rest = new Map();
@@ -79,6 +116,7 @@ console.log(rest);
 console.log(rest.size);
 
 console.log(rest.get(arr));
+*/
 
 /*
 ///////////////////////
