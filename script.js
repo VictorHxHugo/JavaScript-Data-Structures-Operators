a134a940d1c9b08917b70f6aea96e42f6db92802
VictorHxHugo <victorzk6@hotmail.com>
2023-11-30 15:27:06 -0300

    Working With Strings - Part 3 (padding etc)

--- script.js
+++ script.js
@@ -48,8 +48,57 @@ const restaurant = {
   },
 };
 ///////////////////////////////////////
-// Working With Strings - Part 2
+// Working With Strings - Part 3
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
 
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newName = ['Mr', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+  }
+  console.log(namesUpper.join(' '));
+};
+capitalizeName('jessica ann smith davis');
+capitalizeName('victor hugo');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Victor'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(5465475));
+console.log(maskCreditCard(654376734765));
+console.log(maskCreditCard(34565437345735488));
+
+// Repeat
+const message2 = 'Bad waether... All Departues Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+///////////////////////////////////////
+// Working With Strings - Part 2
+/*
 const airline = 'TAP Air Portugal';
 
 console.log(airline.toLowerCase());
@@ -101,7 +150,7 @@ const checkBaggage = function (item) {
 checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
-
+*/
 ///////////////////////////////////////
 // Working With Strings - Part 1
 /*
