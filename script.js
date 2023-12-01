2fbf50e47034235afa5e13364d7816b9c816771d
VictorHxHugo <victorzk6@hotmail.com>
2023-12-01 14:20:41 -0300

    Flight info process w/ str methods

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const openingHours = {
   thu: {
     open: 12,
@@ -792,7 +788,7 @@ Afterwards, test with your own test data!
 
 GOOD LUCK 😀
 */
-
+/*
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 const text = document.querySelector('textarea').value;
@@ -811,3 +807,25 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
+*/
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(40);
+  console.log(output);
+}
